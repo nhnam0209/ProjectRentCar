f398b9d1bfe7f15c46976be5e14a8834a4dbdd61
TuanVo154 <vohoangtuan154@gmail.com>
2022-12-14 23:12:24 +0700

    Feat:add new style for login,register,navigate

--- components/organisms/NavigationBarDesktop.vue
+++ components/organisms/NavigationBarDesktop.vue
@@ -1,13 +1,14 @@
 <template>
-  <nav class="flex justify-between px-5 mt-2">
-    <NuxtLink to="/">Home.</NuxtLink>
+  <nav class="flex justify-between px-5 pt-0.5 h-16 bg-black text-white">
+    <div class="text-2xl items-center space-x-10 font-medium flex">
+      <NuxtLink class="" to="/">Home</NuxtLink>
+    </div>
     <div class="flex items-center space-x-10 text-2xl font-medium">
       <div class=""><NuxtLink to="/aboutus">About Us</NuxtLink></div>
-      <div class=""><NuxtLink to="/login">Login</NuxtLink></div>
-      <div class=""><NuxtLink to="/register">Register</NuxtLink></div>
-
+      <div class=""><NuxtLink to="/rentcar">Rent Car</NuxtLink></div>
+      <button class= "nav-btn-login p-2"><NuxtLink class="" to="/login">Login</NuxtLink></button>
     </div>
-  </nav>
+  </nav>  
 </template>
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
@@ -16,3 +17,10 @@ import { Component, Vue } from "nuxt-property-decorator";
 })
 export default class extends Vue {}
 </script>
+
+<style>
+.nav-btn-login{
+  border: 1px solid white;  
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
   <Default>
     <template #content>
-      <div class="">
-        <div>
-          <h1 class="uppercase text-center">Welcome to Rental Car</h1>
-        </div>
-        <div>
-          <search-form class="mx-auto "/>
-        </div>
-      </div>
     </template>
   </Default>
 </template>
