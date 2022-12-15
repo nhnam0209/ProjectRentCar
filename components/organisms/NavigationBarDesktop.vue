48d78076acfed142677db2607a15226dfacfd072
TuanVo154 <vohoangtuan154@gmail.com>
2022-12-16 02:00:19 +0700

    Feat: add new style + responsive for navigation bar + install fontawesome

--- components/organisms/NavigationBarDesktop.vue
+++ components/organisms/NavigationBarDesktop.vue
@@ -1,13 +1,27 @@
 <template>
-  <nav class="flex justify-between px-5 pt-0.5 h-16 bg-black text-white">
-    <div class="text-2xl items-center space-x-10 font-medium flex">
-      <NuxtLink class="" to="/">Home</NuxtLink>
-    </div>
-    <div class="flex items-center space-x-10 text-2xl font-medium">
-      <div class=""><NuxtLink to="/aboutus">About Us</NuxtLink></div>
-      <button class= "nav-btn-login p-2"><NuxtLink class="" to="/login">Login</NuxtLink></button>
-    </div>
-  </nav>  
+  <nav class="navbar flex justify-between items-center px-4">
+      <div class="logo">
+      </div>
+      <div class="navbar-route flex justify-center items-center">
+        <div class="navbar-list">
+          <NuxtLink class="navbar-items" to="/">Home</NuxtLink>
+        </div>
+        <div class="navbar-list">
+          <NuxtLink class="navbar-items" to="/aboutus">About Us</NuxtLink>
+        </div>
+        <div class="navbar-list navbar-signin">
+          <NuxtLink class="navbar-items" to="/login">Sign In</NuxtLink>
+        </div>
+        <div class="navbar-list">
+          <NuxtLink class="navbar-items" to="/register">Sign Up</NuxtLink>
+        </div>
+      </div>
+      <div class="navbar-list-icons flex items-center justify-center">
+        <fa class="navbar-icons" :icon="['fas', 'house']" />
+        <fa class="navbar-icons" :icon="['fas', 'right-to-bracket']" />
+        <fa class="navbar-icons" :icon="['fas', 'registered']" />
+      </div>
+    </nav>
 </template>
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
@@ -18,8 +32,75 @@ export default class extends Vue {}
 </script>
 
 <style>
-.nav-btn-login{
-  border: 1px solid white;  
+.navbar{
+  height: 60px;
+}
+
+.logo{
+  width: 100px;
+  height: 60px;
+  background: url(../../static/img/logo.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.navbar-signin{
+  position: relative;
+}
+
+.navbar-signin::before{
+  content: "";
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  right: 0;
+  border-right: 2px solid black;
+}
+
+.navbar-items{
+  font-size: 1.5rem;
+  line-height: 2rem;  
+  position: relative;
+  padding-bottom: 4px;
+  margin: 0 30px;
+}
+
+.navbar-items::before{
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3px;
+  width: 0%;
+  background: #00a550;
+  border-radius: 12px;
+  transition: all 0.5s ease;
+}
+
+.navbar-items:hover::before{
+  width: 100%;
+}
+
+.navbar-list-icons{
+  display: none;
+}
+
+.navbar-icons{
+  font-size: 30px;
+  margin: 0 20px;
+}
+
+@media(max-width: 820px){
+.navbar-route{
+  display: none ;
+}
+
+.navbar-list-icons{
+  display: flex;
+}
+
 }
 
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -44,8 +44,18 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/fontawesome',
   ],
+
+  fontawesome: {
+    component: 'fa',
+    icons: {
+      solid: true,
+      brands: true,
+      regular: true,
+    },
+},
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -54,6 +64,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    
   },
 
   publicRuntimeConfig:{
