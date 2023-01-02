b7bc3ea1a28a0afd4cff787140744f2fac6e53b3
TuanVo154 <vohoangtuan154@gmail.com>
2023-01-02 07:52:58 +0700

    -f

--- components/organisms/LoginForm.vue
+++ components/organisms/LoginForm.vue
@@ -34,7 +34,7 @@
         <div class="forget-pass text-gray-400 cursor-pointer my-3">
           Forgot Password?
         </div> -->
-        <RButton nameBtn="Sign In" class="btn-assent"></RButton>
+        <RButton nameBtn="Sign In" class="btn-assent w-full"></RButton>
         <div class="register-link my-5 text-lg text-gray-500">
           No account yet?
           <a href="/register" class="register-item text-black">Sign Up</a>
