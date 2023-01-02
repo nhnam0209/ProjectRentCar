4b6269c02c3df1984cb88f04bbfc0ab37362da3b
TuanVo154 <vohoangtuan154@gmail.com>
2023-01-02 07:52:24 +0700

    -f

---
+++ components/atoms/RLabel.vue
@@ -0,0 +1,22 @@
+<template>
+    <label class="label-input top-2/4 absolute text-lg">{{ nameLabel }}</label>
+</template>
+  
+<script lang="ts">
+import {Component,Emit,Prop,Vue} from 'nuxt-property-decorator';
+@Component({
+    name: "RLabel"
+})
+export default class extends Vue {
+    @Prop({type: String}) nameLabel !:string
+}
+</script>
+
+<style>
+.label-input {
+  top: 0px;
+  left: 0px;
+  color: #b2b2b2;
+}
+</style>
+
