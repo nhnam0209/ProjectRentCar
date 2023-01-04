12182c3abf5446910e288a1d36ec7945bb7bc05e
Nguyễn Hoàng Nam <93809958+nhnam0209@users.noreply.github.com>
2023-01-04 09:50:05 +0700

    HotFix: fix login form

--- components/organisms/LoginForm.vue
+++ components/organisms/LoginForm.vue
@@ -12,32 +12,29 @@
       </header>
       <form class="login-form px-10 box-border" @submit.prevent="submitLogin()">
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="text"
+          <input
+            type="text"
             v-model="user.username"
-            class="input-focus"
+            class="login-input w-full px-1.5 h-10 text-lg border-none outline-none bg-none"
             required
           />
-          <RLabel nameLabel="Username:"></RLabel>
+          <label class="label-input top-2/4 absolute text-lg">Username:</label>
         </div>
 
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="password"
+          <input
+            type="password"
             v-model="user.password"
-            class="input-focus"
+            class="login-input w-full px-1.5 h-10 text-lg border-none outline-none bg-none"
             required
           />
-          <RLabel nameLabel="Password:"></RLabel>
+          <label class="label-input top-2/4 absolute text-lg">Password:</label>
         </div>
         <!-- 
         <div class="forget-pass text-gray-400 cursor-pointer my-3">
           Forgot Password?
         </div> -->
-        <button
-          type="submit"
-          class="w-full btn text-lg text-white font-bold my-7 rounded-xl p-3"
-        >
+        <button type="submit" class="w-full btn text-lg text-white font-bold">
           Sign In
         </button>
         <div class="register-link my-5 text-lg text-gray-500">
@@ -56,29 +53,24 @@ import { Component, Vue } from "nuxt-property-decorator";
 })
 export default class extends Vue {
   msg = "";
-
   async submitLogin() {
     try {
       this.$vxm.user.handleLogin();
+      setTimeout("location.reload(true)", 100);
     } catch (error: any) {
       this.msg = error;
-      console.log(this.msg);
       alert(this.msg);
     }
   }
-
   get user() {
     return this.$vxm.user.userLogin;
   }
-
   set user(value: any) {
     this.$vxm.user.setUserLogin(value);
   }
-
   get isAdmin() {
     return this.$vxm.user.isAdmin;
   }
-
   set isAdmin(value: any) {
     this.$vxm.user.setIsAdmin(value);
   }
@@ -89,11 +81,9 @@ export default class extends Vue {
 .modal {
   position: fixed;
 }
-
 .modal.open {
   display: flex;
 }
-
 .modal-container {
   width: 900px;
   animation: slidein 1s ease;
@@ -101,12 +91,10 @@ export default class extends Vue {
 .modal-header {
   z-index: 1;
 }
-
 .bg-header-login {
   background-image: url(../../static/img/login-cover.jpg);
   height: 200px;
 }
-
 .modal-header::before {
   content: "";
   position: absolute;
@@ -118,70 +106,69 @@ export default class extends Vue {
   top: 0;
   left: 0;
 }
-
 .modal-body {
   padding: 16px;
 }
-
 .border-bottom {
   border-bottom: 1px solid #b2b2b2;
 }
-
 .label-input {
   top: 0px;
   left: 0px;
   color: #b2b2b2;
 }
-
+.login-input:focus ~ .label-input,
+.login-input:valid ~ .label-input {
+  top: -20px;
+  color: #00a550;
+}
 .forget-pass {
   width: 130px;
 }
-
 .forget-pass:hover {
   text-decoration: underline;
 }
-
+.btn {
+  height: 50px;
+  background: #00a550;
+}
+.btn:hover {
+  background: #009548;
+  transition: 0.5s;
+}
 .register-item:hover {
   text-decoration: underline;
   color: #00a550;
 }
-
 @keyframes slidein {
   from {
     transform: translateY(-100%);
     opacity: 0;
   }
-
   to {
     opacity: 1;
     transform: translateY(0%);
   }
 }
-
 @media (max-width: 820px) {
   .modal-container {
     width: 70%;
   }
 }
-
 @media (max-width: 430px) {
   .modal-container {
     height: 70%;
   }
-
   .bg-header-login {
     background-color: #000;
     background: none;
   }
-
   .title-header-modal {
     font-size: 26px;
   }
-
   .label-input {
     font-size: 18px;
   }
-
   .register-link {
     font-size: 18px;
   }
