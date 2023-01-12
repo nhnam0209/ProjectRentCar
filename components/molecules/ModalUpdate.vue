cc228f0c07dd98a8d0ab04e13c53462531ca1c34
Nguyễn Hoàng Nam <93809958+nhnam0209@users.noreply.github.com>
2023-01-12 16:15:09 +0700

    Feat: Handle update in admin

--- components/molecules/ModalUpdate.vue
+++ components/molecules/ModalUpdate.vue
@@ -1,67 +1,158 @@
 <template>
   <div class="background-modal block">
-    <form action="" class="signup-form justify-center items-center bg-white rounded-xl shadow" :class="{'modal-close': isActive}">
+    <form
+      action=""
+      class="signup-form justify-center items-center bg-white rounded-xl shadow"
+      :class="{ 'modal-close': isActive }"
+    >
       <div class="p-6">
         <header class="title-signup text-3xl font-bold mt-7">
-          <span>Add More Users</span>
+          <span>Update User Information</span>
         </header>
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="text"
+          <input
+            type="text"
             class="input-focus"
+            v-model="user.first_name"
+            :placeholder="userInfo.first_name"
           />
-          <label class="label-input top-2/4 absolute text-lg">First name:</label>
+          <label class="label-input top-2/4 absolute text-lg"
+            >First name:</label
+          >
         </div>
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="text"
+          <input
+            type="text"
             class="input-focus"
+            v-model="user.last_name"
+            :placeholder="userInfo.last_name"
           />
           <label class="label-input top-2/4 absolute text-lg">Last name:</label>
         </div>
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="email"
+          <input
+            type="email"
             class="input-focus"
+            v-model="user.email"
+            :placeholder="userInfo.email"
           />
           <label class="label-input top-2/4 absolute text-lg">Email:</label>
         </div>
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="text"
+          <input
+            type="text"
             class="input-focus"
+            v-model="user.username"
+            :placeholder="userInfo.username"
           />
           <label class="label-input top-2/4 absolute text-lg">Username:</label>
         </div>
         <div class="relative border-bottom my-7">
-          <RInput
-            typeInput="password"
+          <input
+            type="text"
             class="input-focus"
+            v-model="user.phone"
+            :placeholder="userInfo.phone_number"
           />
-          <label class="label-input top-2/4 absolute text-lg">Password:</label>
+          <label class="label-input top-2/4 absolute text-lg">Phone:</label>
+        </div>
+        <div class="relative border-bottom my-7">
+          <input
+            type="text"
+            class="input-focus"
+            v-model="user.id_card"
+            :placeholder="userInfo.id_card"
+          />
+          <label class="label-input top-2/4 absolute text-lg">ID Card:</label>
+        </div>
+        <div class="relative my-7 inline-flex">
+          <label class="top-2/4 text-lg">Gender:</label>
+          <select
+            name="gender"
+            id="gender"
+            v-model="user.gender"
+            :placeholder="userInfo.gender"
+          >
+            <option value="" disabled>Please Select</option>
+            <option
+              v-for="gender in genders"
+              :key="gender.id"
+              :value="gender.value"
+            >
+              {{ gender.name }}
+            </option>
+          </select>
+        </div>
+        <div class="relative border-bottom my-7">
+          <input
+            type="text"
+            class="input-focus"
+            v-model="user.driven_license"
+            :placeholder="userInfo.driven_license"
+          />
+          <label class="label-input top-2/4 absolute text-lg"
+            >Driving License:</label
+          >
         </div>
       </div>
       <div class="button-group flex justify-between w-full px-7">
-          <RButton class="btn-assent" nameBtn="Update"></RButton>
-          <RButton class="btn-close" nameBtn="Close"></RButton>
+        <RButton
+          class="btn-assent"
+          nameBtn="Update"
+          @click="handleUpdateUserInfo(user)"
+        ></RButton>
+        <RButton class="btn-close" nameBtn="Close"></RButton>
       </div>
     </form>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
+import { Component, Prop, Vue } from "nuxt-property-decorator";
 @Component({
   name: "ModalUpdate",
 })
 export default class extends Vue {
+  @Prop({}) userInfo!: any;
   isActive: Boolean = false;
-  
+  genders = [
+    {
+      id: "1",
+      name: "Male",
+      value: "Male",
+    },
+    {
+      id: "2",
+      name: "Female",
+      value: "Female",
+    },
+    {
+      id: "3",
+      name: "Others",
+      value: "Others",
+    },
+  ];
+
+  handleUpdateUserInfo(user: any) {
+    try {
+      this.$vxm.user.setUser(this.userInfo);
+      this.$vxm.user.updateUserAdmin(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+  get user() {
+    return this.$vxm.user.userInfo;
+  }
+
+  set user(value: any) {
+    this.$vxm.user.setUserInfo(value);
+  }
 }
 </script>
 
 <style>
-.background-modal{
+.background-modal {
   position: fixed;
   right: 0;
   left: 0;
@@ -73,5 +164,4 @@ export default class extends Vue {
   display: flex;
   z-index: 2;
 }
-
 </style>
